bd257b087c7d55c3370a2cf61a7646bcca1f419f
Nathishwar <nathishwarc@gmail.com>
2025-11-19 10:53:10 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// For Netlify deployment, we need to use the correct API URL
 const API = axios.create({ 
   baseURL: import.meta.env.VITE_API_URL || 'https://backendtodo-nk7m.onrender.com/api' 
 });
